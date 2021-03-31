61d2b97aea6ea2c5fc248e481bcc7182dd3c9920
Falxns <maks.falonse@gmail.com>
2021-03-31 19:37:47 +0300

    feat: add server and games connection with mongodb

--- client/src/components/GameList/gameList.jsx
+++ client/src/components/GameList/gameList.jsx
@@ -2,48 +2,6 @@ import "./gameList.css";
 import React, { Component } from "react";
 import GameCard from "../GameCard/gameCard";
 
-const fetchGames = async () => {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve([
-        {
-          _id: 123,
-          title: "Genshin Impact",
-          platform: "PC",
-          genre: "Shooter",
-          maturity: 4,
-          price: 5,
-          desc: "jkfdhsjfhgjkf",
-          imageUrl:
-            "http://localhost:3000/images/games/605a64d709b1740e7d86c84a.jpg",
-        },
-        {
-          _id: 234,
-          title: "Genshin Impact",
-          platform: "PC",
-          genre: "Shooter",
-          maturity: 4,
-          price: 0,
-          desc: "jkfdhsjfhgjkf",
-          imageUrl:
-            "http://localhost:3000/images/games/605a64d709b1740e7d86c84a.jpg",
-        },
-        {
-          _id: 345,
-          title: "Genshin Impact",
-          platform: "PC",
-          genre: "Shooter",
-          maturity: 4,
-          price: 0,
-          desc: "jkfdhsjfhgjkf",
-          imageUrl:
-            "http://localhost:3000/images/games/605a64d709b1740e7d86c84a.jpg",
-        },
-      ]);
-    }, 3000)
-  );
-};
-
 class GameList extends Component {
   state = {
     games: [],
@@ -51,19 +9,20 @@ class GameList extends Component {
   };
 
   componentDidMount() {
-    // fetch("localhos...")
-    fetchGames()
-      .then((games) => {
-        this.setState({ games, loading: false });
+    fetch("http://localhost:3000/games")
+      .then((res) => {
+        res.json().then((games) => {
+          this.setState({ games, loading: false });
+        });
       })
       .catch((err) => console.log(err));
-    this.setState({ games: [] });
   }
 
   renderGames() {
-    if (this.state.loading) return <h5>Loading...</h5>;
+    if (this.state.loading) return <h5 className="games-text">Loading...</h5>;
 
-    if (this.state.games.length === 0) return <h5>There are no games :c</h5>;
+    if (this.state.games.length === 0)
+      return <h5 className="games-text">There are no games :c</h5>;
 
     return this.state.games.map((game) => (
       <GameCard key={game._id} gameData={game} />
---
+++ server/index.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const formData = require("express-form-data");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb://localhost/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongodb."))
+  .catch(() => console.log("Error connecting to mongodb."));
+
+const gameSchema = new mongoose.Schema({
+  title: String,
+  imageUrl: String,
+  platform: String,
+  genre: String,
+  maturity: Number,
+  price: Number,
+  desc: String,
+});
+const Game = mongoose.model("Game", gameSchema, "games");
+
+const app = express();
+app.use(express.static("public"));
+app.use(cors());
+
+app.use(formData.parse());
+app.use(formData.format());
+app.use(formData.stream());
+app.use(formData.union());
+
+app.post("/game", function (req, res) {
+  const { title, platform, genre, maturity, price, desc, image } = req.body;
+
+  let imageName = "";
+  if (image) {
+    imageName = mongoose.Types.ObjectId().toHexString() + ".jpg";
+    const imagePath = "./public/images/games/" + imageName;
+
+    fs.writeFileSync(
+      path.join(process.cwd(), imagePath),
+      fs.readFileSync(image.path)
+    );
+  } else {
+    imageName = "default-img.png";
+  }
+
+  const game = new Game({
+    title,
+    platform,
+    genre,
+    maturity,
+    price,
+    desc,
+    imageUrl: "http://localhost:3000/images/games/" + imageName,
+  });
+
+  game
+    .save()
+    .then(() => res.redirect("http://localhost:3000/game/" + game._id))
+    .catch(() => console.log("500"));
+});
+
+app.get("/filter", function (req, res) {
+  const platform = req.query.platform;
+  const genre = req.query.genre;
+  const maturity = req.query.maturity;
+  Game.find({ platform, genre, maturity })
+    .lean()
+    .then((games) => {
+      res.render("index", { games });
+    })
+    .catch(() => res.send("404"));
+});
+
+app.get("/games", function (req, res) {
+  Game.find()
+    .lean()
+    .then((games) => {
+      res.send(games);
+    })
+    .catch(() => res.status(503).send());
+});
+
+app.get("/game/:id", function (req, res) {
+  const gameID = req.params.id;
+
+  Game.findById(gameID)
+    .then((game) =>
+      res.render("game", {
+        title: game.title,
+        imageUrl: game.imageUrl,
+        platform: game.platform,
+        genre: game.genre,
+        maturity: game.maturity,
+        price: game.price,
+        desc: game.desc,
+      })
+    )
+    .catch(() => res.send("404"));
+});
+
+app.get("/delete/:id", function (req, res) {
+  const gameID = req.params.id;
+
+  Game.findByIdAndDelete(gameID)
+    .then(() => res.redirect("/"))
+    .catch(() => res.send("404"));
+});
+
+app.listen(3000, () => console.log("Server is listening.."));
