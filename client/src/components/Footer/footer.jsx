b641611ca13581267f511c1f0f3e49010f5c6c82
Falxns <maks.falonse@gmail.com>
2021-05-01 14:34:59 +0300

    refactor: update styles of footer to match BEM

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -6,33 +6,33 @@ import twitter from "../../assets/icons/twitter.svg";
 
 const Footer = () => {
   return (
-    <footer>
-      <p className="footer-p">Contact us:</p>
+    <footer className="footer">
+      <p className="footer__p">Contact us:</p>
       <a
         href="https://vk.com"
         target="_blank"
         rel="noreferrer noopener"
-        className="img-anchor"
+        className="footer__a"
       >
-        <img className="footer-icon" src={vk} alt="VK" />
+        <img className="footer__img" src={vk} alt="vk" />
       </a>
       <a
         href="https://www.instagram.com"
         target="_blank"
         rel="noreferrer noopener"
-        className="img-anchor"
+        className="footer__a"
       >
-        <img className="footer-icon" src={inst} alt="Instagram" />
+        <img className="footer__img" src={inst} alt="instagram" />
       </a>
       <a
         href="https://twitter.com"
         target="_blank"
         rel="noreferrer noopener"
-        className="img-anchor"
+        className="footer__a"
       >
-        <img className="footer-icon" src={twitter} alt="Twitter" />
+        <img className="footer__img" src={twitter} alt="twitter" />
       </a>
-      <p className="footer-p">©2021</p>
+      <p className="footer__p">©2021</p>
     </footer>
   );
 };
