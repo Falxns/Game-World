f7908acab52de14baf9f15fc87393b4140c7377a
Falxns <maks.falonse@gmail.com>
2021-05-01 14:23:40 +0300

    refactor: update styles in header

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -15,30 +15,33 @@ const Header = () => {
 
   const actions = user ? (
     <>
-      <button onClick={handleLogout} className="header__button">
+      <button
+        onClick={handleLogout}
+        className="header__a_button header__button"
+      >
         Logout
       </button>
-      <Link to="/addgame" className="img-anchor">
-        <img className="img-header" src={plusIcon} alt="Add" />
+      <Link to="/addgame" className="header__a header__a_icon">
+        <img className="header__img" src={plusIcon} alt="add" />
       </Link>
     </>
   ) : (
     <>
-      <Link to="/login">
+      <Link to="/login" className="header__a_button">
         <button className="header__button">Login</button>
       </Link>
-      <Link to="/registration">
+      <Link to="/registration" className="header__a_button">
         <button className="header__button">Sign up</button>
       </Link>
     </>
   );
 
   return (
-    <header>
-      <Link to="/" className="img-anchor">
-        <img className="img-header" src={homeIcon} alt="Home" />
+    <header className="header">
+      <Link to="/" className="header__a header__a_icon">
+        <img className="header__img" src={homeIcon} alt="home" />
       </Link>
-      <h3 className="header-title">Game World</h3>
+      <h3 className="header__title">Game World</h3>
       <div className="header__div">{actions}</div>
     </header>
   );
