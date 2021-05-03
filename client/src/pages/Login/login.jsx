c3eb4d84658a95f09cff9d18c76cba039c34e9e4
Falxns <maks.falonse@gmail.com>
2021-05-03 17:52:45 +0300

    refactor: update styles in login page

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -47,8 +47,8 @@ class Login extends Component {
   render() {
     return (
       <>
-        <h1 className="head-line">Login</h1>
-        <div className="login-container">
+        <h1 className="login__header">Login</h1>
+        <div className="login__container">
           <label className="container__label" htmlFor="email">
             Email:
           </label>
